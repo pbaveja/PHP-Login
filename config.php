e05d3fa9d1bbe312884e5e639625b4ba127e2c7c
Prakhar Baveja <prakhar.baveja@gmail.com>
2021-08-31 15:07:26 +0530

    update config.php to handle multiple users

--- config.php
+++ config.php
@@ -1,5 +1,15 @@
 <?php
 
 /* Define username and password */
-$Username = 'Steve';
-$Password = '$2y$10$1GmNO63bbKWpaPxcqLaLW.yVmvoxyOD9krWXxn2XAY.QSdbfcARDK';
+$users = [
+	[
+		'username' => 'Test',
+		'password' => '$2y$10$NQUZbplW/Fhpl9OSxeECUO4DMsFpNOw42qngGuVsMvrxTb76GNrlG' // password
+	],
+	[
+		'username' => 'Shivam',
+		'password' => '$2y$10$NQUZbplW/Fhpl9OSxeECUO4DMsFpNOw42qngGuVsMvrxTb76GNrlG' // password
+	],
+];
+// $Username = 'Steve';
+// $Password = '$2y$10$1GmNO63bbKWpaPxcqLaLW.yVmvoxyOD9krWXxn2XAY.QSdbfcARDK';
